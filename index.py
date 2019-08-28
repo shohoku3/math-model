9a7ac5c400a57f78a25bbae9023b5fd35499b4f0
shohoku3 <syf19961002@outlook.com>
2019-08-28 12:39:24 +0800

    add

--- index.py
+++ index.py
@@ -21,7 +21,7 @@ for row in Datareader:
 			Lines.append(['0',row[2][0:-1]])
 	except Exception as e:	
 		#print(e)
-		print('error')
+		print(e)
 #参数检验		
 '''
 @param start 
@@ -57,36 +57,24 @@ def searchLine(start,end,Lines):
 					Line_tmp2=''.join(Lines[i][3]).split('-')
 					for j in range(len(Line_tmp1)):
 						if str(start)==Line_tmp1[j][1:]:
-							#print('success search ' + str(start) +' from up')
-							#print('Line'+str(i+1))
 							startLine.append(i+1)
 						elif str(end)==Line_tmp1[j][1:]:
-							#print('success search '+str(end)+' from up')
-							#print('Line'+str(i+1))
 							endLine.append(i+1)
 					for j in range(len(Line_tmp2)):
 						if str(start)==Line_tmp2[j][1:]:
-							#print('success search '+ str(start) +' from down')
-							#print('Line'+str(i+1))
 							startLine.append(i+1)
 						elif str(end)==Line_tmp2[j][1:]:
-							#print('success search '+str(end)+' from down')
-							#print('Line'+str(i+1))
 							endLine.append(i+1)
 				else:
 					Line_tmp=''.join(Lines[i][1]).split('-')
 					for k in range(len(Line_tmp)):
 						if str(start)==Line_tmp[k][1:]:
-							#print('success serach '+str(start))
-							#print('Line'+str(i+1))
 							startLine.append(i+1)
 						elif str(end)==Line_tmp[k][1:]:
-							#print('success search ' +str(end))
-							#print('Line'+str(i+1))
 							endLine.append(i+1)
 			except Exception as e:
-				#print(e)
-				print('error')
+				print(e)
+		print('--------通过起点终点找寻到的路线有---------')
 		print(list(set(startLine)),list(set(endLine)))
 		resline=[]
 		resline.append(list(set(startLine)))
@@ -110,27 +98,98 @@ def serachFromTwoLines(startLine,endLine):
 
 # 用来把路线转换为节点列表的函数
 def convertNode(Line):
-	if len(Line)>2:
+	if len(Line)!=2:
 		nodeUp=''.join(Line[1]).split('-')
 		nodeDown=''.join(Line[3]).split('-')
 		return nodeUp,nodeDown
-	if len(Line)==2:
+	elif len(Line)==2:
 		node=''.join(Line[1]).split('-')
 		return node
 
-# 通过寻找线路上的站点 看是否有相同站点 即为换乘站点
+# 寻找线路上的站点 看是否有相同站点 即为换乘站点
 def serachEqualNode(startLine,endLine):
-	print(len(Lines[351]))
 	for i in range(len(startLine)):
 		startLineNode=convertNode(Lines[startLine[i]-1])
-	for j in range(len(endLine)):
-		endLineNode=convertNode(Lines[endLine[j]-1])
-	print(startLineNode,endLineNode)
+		for j in range(len(endLine)):
+			endLineNode=convertNode(Lines[endLine[j]-1])
+			if EqualNode(startLineNode,endLineNode):
+				print('------------尝试寻找一次换乘路线-----------')
+				print('L0'+str(startLine[i]),'L0'+str(endLine[j]))
+				resnode=EqualNode(startLineNode,endLineNode)
+				print(list(set(resnode)))
+
+# 通过寻找线路上的站点 看是否有相同站点 即为换乘站点
+def EqualNode(startLineNode,endLineNode):
+	resnode=[]
+	if isinstance(startLineNode,list) & isinstance(endLineNode,list):
+		for i in startLineNode:
+			for j in endLineNode:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+	elif isinstance(startLineNode,tuple) &isinstance(endLineNode,list):
+		for i in endLineNode:
+			for j in startLineNode[0]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+		for i in endLineNode:
+			for j in startLineNode[1]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+	elif isinstance(startLineNode,list) &isinstance(endLineNode,tuple):
+		for i in startLineNode:
+			for j in endLineNode[0]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+		for i in startLineNode:
+			for j in endLineNode[1]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+	elif isinstance(startLineNode,tuple) &isinstance(endLineNode,tuple):
+		for i in startLineNode[0]:
+			for j in endLineNode[0]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+		for i in startLineNode[0]:
+			for j in endLineNode[1]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+		for i in startLineNode[1]:
+			for j in endLineNode[0]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+		for i in startLineNode[1]:
+			for j in endLineNode[1]:
+				if i==j:
+					resnode.append(i)
+				else:
+					pass
+	if len(resnode)!=0:
+		return resnode
+		return True
+
+
 # main function
 resline=searchLine(3359,1828,Lines)
+print('----------------尝试寻找直达路径-----------')
 if serachFromTwoLines(resline[0],resline[1]):
 	i=serachFromTwoLines(resline[0],resline[1])
-	print(i)
+	resnode.append(i)
 	print('找到直达路径')
 else:
 	print('未找到直达路径')
